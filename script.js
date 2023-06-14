02cddffec1d77775d6de02a13d70068f37c21ed1
Ashish <chakravartiashish2406@gmail.com>
2023-06-14 14:38:28 +0530

    final work save

---
+++ script.js
@@ -0,0 +1,33 @@
+gsap.from(".nav h4",{
+
+    y:-50,
+    duration:1,
+    delay:0.2,
+    stagger:0.3
+})
+gsap.from(".main h1",{
+
+    x:-600,
+    duration:1,
+    delay:1.5,
+    stagger:0.4,
+    opacity:0,
+})
+gsap.from(".main img",{
+
+    x:600,
+    duration:1,
+    delay:3,
+    stagger:0.4,
+    opacity:0,
+    rotate:0,
+})
+
+gsap.from(".bottom h4",{
+    y:100,
+    duration:1,
+    delay:3.5,
+    stagger:0.3,
+    opacity:0,
+})
+
